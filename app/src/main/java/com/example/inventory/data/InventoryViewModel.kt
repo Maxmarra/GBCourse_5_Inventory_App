e86983294abbdff7b4787c34935b1f22fe508610
Maxmarra <5919669@gmail.com>
2022-06-06 13:32:51 +0300

    1

--- app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.inventory.data
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +12,37 @@ class InventoryViewModel @Inject constructor(
 ): ViewModel() {
 
     val items = repository.getItemsRepo()
+
+    private fun insertItem(item: Item) {
+        viewModelScope.launch {
+            repository.insertRepo(item)
+        }
+    }
+    /* ЭМУЛЯЦИЯ ПОЛУЧЕНИЯ И ДОБАВЛЕНИЯ ДАННЫХ В БАЗУ */
+    //из полученных вводных данных в полях приложения
+    //собираем Объект типа Item
+    //и возвращаем уже готовый объект Item
+    private fun getNewItemEntry(
+        itemName: String,
+        itemPrice: String,
+        itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice = itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    //имея вводные данные, через getNewItemEntry()
+    //собираем, возвращаем готовый объект Item
+    //и после вносим его в базу данных
+    fun addNewItem(
+        itemName: String,
+        itemPrice: String,
+        itemCount: String
+    ) {
+        val newItem = getNewItemEntry(itemName, itemPrice, itemCount)
+        insertItem(newItem)
+    }
 
 }
